9c4a5bab5a842db7beebe3154c5ef1314cac9da4
wnf <william@fluxrme.com>
2023-10-10 16:57:43 -0500

    now does basic checking: first play (horizontal & cove
    ring center square); all plays are either in a single column or a single row. handles turn switching so that when plays
    fail checks they are removed from the board, letters are returned to players letter tray, and turn passes to next player

--- script.js
+++ script.js
@@ -32,6 +32,23 @@
         //_: {count: 2, points: 0},
     };
 
+    const blankBoard = [
+        ['','','','','','','','','','','','','','',''],
+        ['','','','','','','','','','','','','','',''],
+        ['','','','','','','','','','','','','','',''],
+        ['','','','','','','','','','','','','','',''],
+        ['','','','','','','','','','','','','','',''],
+        ['','','','','','','','','','','','','','',''],
+        ['','','','','','','','','','','','','','',''],
+        ['','','','','','','','','','','','','','',''],
+        ['','','','','','','','','','','','','','',''],
+        ['','','','','','','','','','','','','','',''],
+        ['','','','','','','','','','','','','','',''],
+        ['','','','','','','','','','','','','','',''],
+        ['','','','','','','','','','','','','','',''],
+        ['','','','','','','','','','','','','','',''],
+        ['','','','','','','','','','','','','','','']		
+    ]
     
 /*----- state variables -----*/
 
@@ -42,8 +59,8 @@
     let refillLettersButtonClicked = false //turns true once button is clicked so it can be hidden
     let selectedLetters = []; //letters that have been clicked and will either be going on the board or back into the letterbag
     let placedLetters = []; //letters that were placed during the current turn
-    let exchangingLetters = false; //toggle variable that tells us whether the player is in the middle of a letter exchange, to help put guardrails on functions
-
+    let exchangingLetters = false //toggle variable that tells us whether the player is in the middle of a letter exchange, to help put guardrails on functions
+    let boardAsOfEndOfLastTurn = blankBoard
     const players = [
         {name: 'One', turn: 0, score: 0, letters: []},
         {name: 'Two', turn: 1, score: 0, letters: []}
@@ -59,7 +76,6 @@
         for (let i = 0; i < count; i++) { // Push the letter into the array 'count' number of times
             letterBag.push(letter);
         }
-        //console.log(letterBag)
     }
     
     
@@ -95,23 +111,7 @@
 
     function init () {
         //Rotate 90 degrees counter-clockwise and array is visualization of the board
-        board = [
-            ['','','','','','','','','','','','','','',''],
-            ['','','','','','','','','','','','','','',''],
-            ['','','','','','','','','','','','','','',''],
-            ['','','','','','','','','','','','','','',''],
-            ['','','','','','','','','','','','','','',''],
-            ['','','','','','','','','','','','','','',''],
-            ['','','','','','','','','','','','','','',''],
-            ['','','','','','','','','','','','','','',''],
-            ['','','','','','','','','','','','','','',''],
-            ['','','','','','','','','','','','','','',''],
-            ['','','','','','','','','','','','','','',''],
-            ['','','','','','','','','','','','','','',''],
-            ['','','','','','','','','','','','','','',''],
-            ['','','','','','','','','','','','','','',''],
-            ['','','','','','','','','','','','','','','']		
-        ]
+        board = blankBoard
 
         turn = 1
         round = 1
@@ -123,6 +123,8 @@
         players[1]['letters'] = []
         selectedLetters = []
         placedLetters = []
+        refillLettersButtonClicked = false
+        exchangingLetters = false
     }
 
 //Visualize all state in the DOM
@@ -160,7 +162,6 @@
         )
     }
 
-
     function renderScores(){     
         playerScores[0].innerText = players[0]['score']
         playerScores[1].innerText = players[1]['score']
@@ -169,7 +170,7 @@
 
     function renderControls(){
             
-        submitPlayButton.style.visibility = !winner ? 'visible':'hidden'
+        submitPlayButton.style.visibility = !winner && !exchangingLetters ? 'visible':'hidden'
         refillLettersButton.style.visibility = refillLettersButtonClicked ? 'hidden':'visible'
         playAgainButton.style.visibility = winner ? 'visible':'hidden'
 
@@ -188,7 +189,12 @@
 
     function renderLettersInTray(){
         letterTray.innerHTML = ''
+        let player
         if(turn === 1){player = players[0]}else{player = players[1]} 
+        // Debugging
+        // console.log(player); // Check the content of the player object
+        // console.log(Array.isArray(player.letters)); // Check if player.letters is an array
+
         player.letters.forEach(
             function(letter){
                 const letterTile = document.createElement('div')
@@ -200,8 +206,12 @@
     }
 
     function renderNumberOfLettersLeftInBag(){
-        const numberofLettersLeftInBag = letterBag.length
-        document.getElementById('number-of-letters-left-in-bag').innerText = numberofLettersLeftInBag
+        const numLettersInLetterBag = letterBag.length
+        document.getElementById('number-of-letters-left-in-bag').innerText = numLettersInLetterBag
+    }
+
+    function renderMessages(){
+        
     }
 
 //User Interactions
@@ -220,20 +230,39 @@
         }
 
         player.letters = [...player.letters, ...refillLetters] //refill the player's letter tray
-        render()
+        renderLettersInTray()
     }
     
     function refillLetterFromBag(){
         const numLettersInLetterBag = letterBag.length
-        const randomIndex = Math.floor(Math.random() * numLettersInLetterBag) + 1;
-        randomLetter = letterBag[randomIndex]
-        letterBag.splice(randomIndex,1) //Take letter OUT of letterBag so it cannot be selected anymore and doesn't count towards numLettersInLetterBag
+        const randomIndex = Math.floor(Math.random() * numLettersInLetterBag)
+        const randomLetter = letterBag[randomIndex]
+        
+        letterBag.splice(randomIndex,1) //Take letter OUT of letterBag so it cannot be selected anymore & doesn't count towards numLettersInLetterBag
         renderNumberOfLettersLeftInBag()
+        
         return(randomLetter)
     }
 
+    function selectLetter(event){
+        if(!exchangingLetters && selectedLetters.length > 0){return} //guard: when not exchanging letters, should not be able to select more than one letter
+        if(placedLetters.length === 0){boardAsOfEndOfLastTurn = JSON.parse(JSON.stringify(board))} //store initial state of board as of end of last turn so can revert to it if player makes an invalid play
+
+        if(turn === 1){player = players[0]}else{player = players[1]} 
+        const currentLetterEls = document.getElementsByClassName('current-player-letter')
+        
+        const currentLetterElsArray = [...currentLetterEls]
+        const letterIdx = currentLetterElsArray.indexOf(event.target)
+        
+        const selectedLetterEl = currentLetterEls[letterIdx] //update letter tray formatting to show that letter has been selected
+        selectedLetterEl.style.backgroundColor = '#636363'
+
+        const selectedLetter = player.letters[letterIdx] //return actual letter as js object
+        selectedLetters.push(selectedLetter)
+    }
+
     function placeLetter(event){
-        if(selectedLetters.length !== 1){return} //guard: if have no selected letters
+        if(selectedLetters.length !== 1){return} //guard: cancel operation if have no or multiple selected letters
 
         const boardCellEl = event.target
         if(!boardCellEl.classList.contains('board-cell')) {return} // guard: if the clicked element is not a valid board cell
@@ -247,72 +276,97 @@
 
         if(turn === 1){player = players[0]}else{player = players[1]} 
 
-        //cleanup
+        //STORAGE FOR USE IN OTHER FUNCTIONS
+        const placedLetter = {letter: selectedLetters[0], xCoord: boardCellColIdx, yCoord: boardCellRowIdx}
+        placedLetters.push(placedLetter) //add placed letter to placedLetters
+        
+        //CLEANUP
         const indexToRemove = player.letters.indexOf(selectedLetters[0])
         player.letters.splice(indexToRemove, 1) //remove the letter from the player's current letters
         renderLettersInTray() //re-render the tray with the letter that was placed now gone
-        
-        const placedLetter = {letter: selectedLetters[0], xCoord: boardCellColIdx, yCoord: boardCellRowIdx}
-        placedLetters.push(placedLetter) //add placed letter to placedLetters
         selectedLetters = [] //reset selectedLetters so placeLetter can be called again
-
-        // console.log(indexToRemove, player.letters)
-
-    }
-
-    function selectLetter(event){
-        if(turn === 1){player = players[0]}else{player = players[1]} 
-        const currentLetterEls = document.getElementsByClassName('current-player-letter')
-        
-        const currentLetterElsArray = [...currentLetterEls]
-        const letterIdx = currentLetterElsArray.indexOf(event.target)
-        
-        const selectedLetterEl = currentLetterEls[letterIdx] //update letter tray formatting to show that letter has been selected
-        selectedLetterEl.style.backgroundColor = '#636363'
-
-        const selectedLetter = player.letters[letterIdx] //return actual letter as js object
-        selectedLetters.push(selectedLetter)
-        // console.log(letterIdx, player.letters, selectedLetter)
     }
 
     function submitPlay(){
-        if(placedLetters === [] && !exchangingLetters){return}
-
-        checkPlay()
-        scorePlay()
-
-
-        //cleanup
-        selectedLetters = [] // reset selectedLetters
-        if(turn === 1){ // update turn and round
-            turn = 2
-        }else{
-            turn = 1
-            round += 1
-        }
+        if(placedLetters.length === 0 && !exchangingLetters){return} //guard: if the player has not placed any letters and is not doing a letter exchange, don't do anything
         
-        // placedLetters = []
-        exchangingLetters = false
-        refillLettersButtonClicked = false
+         
 
-        // console.log(turn, round, exchangingLetters, selectedLetters, players)
+        if(!checkPlay()){
+            
+            //Remove placed letters from board
+            // if(turn === 1 && round === 1){boardAsOfEndOfLastTurn = blankBoard}
+            board = boardAsOfEndOfLastTurn
 
+            let player
+            if(turn === 1){player = players[0]}else{player = players[1]}
+            player.letters = [...player.letters, ...placedLetters.map((element) => element.letter)] //return placed letter to player's tray
+            
+            console.log(player); // Check the content of the player object
+            console.log(player.letters); // Check if player.letters is an array
+
+            endTurnUpdates()
+            render()
+            return    
+        }
+        // scorePlay()
+        
+        
+        endTurnUpdates()
         render()
-        renderLettersInTray()
+        
     }
 
 //Checking the Validity of the Play
     
     function checkPlay(){
-        if(turn === 1 && round === 1){
-
-        }
-
         
+        let checkFirstPlayResult // check whether play is horizontal and covers first square
+        if(turn === 1 && round === 1){
+            checkFirstPlayResult = checkFirstPlay()
+        }else{
+            checkFirstPlayResult = true
+        } 
+
+        const checkStraightPlayResult = checkStraightPlay()
+        const result = checkFirstPlayResult && checkStraightPlayResult
+        // console.log(checkFirstPlayResult, checkStraightPlayResult, result)
+        return(result)
+
+    }
+
+    function checkFirstPlay(){
+        const xCoords = placedLetters.map((item) => item.xCoord)
+        const yCoords = placedLetters.map((item) => item.yCoord)
+
+        const uniqueXCoords = [... new Set(xCoords)]
+        const uniqueYCoords = [... new Set(yCoords)]
+
+        let isHorizontalPlay = false
+        if(uniqueXCoords.length === 1){isHorizontalPlay = true} //check if play is horizontal
+
+        let coversCentralSquare = false
+        if(uniqueXCoords.includes(7) && uniqueYCoords.includes(7)){coversCentralSquare = true}
+
+        const result = isHorizontalPlay && coversCentralSquare
+
+
+        return(result)
+    }
+
+    function checkStraightPlay(){
+        const xCoords = placedLetters.map((item) => item.xCoord)
+        const yCoords = placedLetters.map((item) => item.yCoord)
+
+        const uniqueXCoords = [... new Set(xCoords)]
+        const uniqueYCoords = [... new Set(yCoords)]
+
+        const result = uniqueXCoords.length === 1 || uniqueYCoords.length === 1 //check if play is in either one column or one row
+        return(result)
     }
 
     function checkWordConnected(){
-
+        selectedLetters.forEach(checkLetterConnected())
     }
 
     function checkLetterConnected(){
@@ -321,7 +375,6 @@
 
     function getAdjacentCells(grid, col, row) {
         if(col > 14 || row > 14){return}
-        console.log(col, row)
     
         const adjacentCells = [];
     
@@ -340,7 +393,6 @@
             if (newRow >= 0 && newRow <= 14 && newCol >= 0 && newCol <= 14) {
                 const adjacentCellContents = grid[newCol][newRow]
                 adjacentCells.push({contents: adjacentCellContents, col: newCol, row: newRow});
-                console.log(newCol, newRow)
             }
             
         }
@@ -348,11 +400,8 @@
 
         return adjacentCells;
     }
-    
-    // function 
 
 //Scoring the Play
-
     //!NEEDS TO CALCULATE GIVEN LETTERS OTHER PLAYERS PLACED PREVIOUSLY (ALL WORDS & LETTERS FORMED)
     function scorePlay(){
         
@@ -371,11 +420,21 @@
         
     }
 
+//Ending the Turn
+    function endTurnUpdates(){
+        // boardAsOfEndOfLastTurn = board
+        refillLettersButtonClicked = false //reset refillLettersButoonClicked
+        selectedLetters = [] // reset selectedLetters
+        placedLetters = [] // reset placedLetters
+        if(turn === 1){ // update turn and round
+            turn = 2
+        }else{
+            turn = 1
+            round += 1
+        }
+
+        // console.log(turn, round, players, board)
+    } 
+
 init()
 render()
-
-// board.forEach(function(col, row){
-//     console.log(col, row)
-//     //try{getAdjacentCells(board, col, row)}catch(error){console.log(error.message)}
-// }
-// )
